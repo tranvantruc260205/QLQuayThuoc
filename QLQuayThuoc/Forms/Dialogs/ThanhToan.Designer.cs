172427e059a8ab20f5f9ee4ef83874231f76a0b9
Trần Trúc <tranvantruc260205@gmail.com>
2026-07-23 07:37:24 +0700

    Update dialog ThanhToan

--- QLQuayThuoc/Forms/Dialogs/ThanhToan.Designer.cs
+++ QLQuayThuoc/Forms/Dialogs/ThanhToan.Designer.cs
@@ -36,30 +36,34 @@
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             groupBox2 = new GroupBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             groupBox3 = new GroupBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
+            pictureBox1 = new PictureBox();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
             // 
             groupBox1.BackColor = SystemColors.ControlLight;
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(26, 12);
+            groupBox1.Location = new Point(22, 10);
+            groupBox1.Margin = new Padding(2, 2, 2, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1683, 105);
+            groupBox1.Padding = new Padding(2, 2, 2, 2);
+            groupBox1.Size = new Size(1402, 88);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Thông tin ";
@@ -67,9 +71,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(24, 52);
+            label1.Location = new Point(20, 43);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(68, 30);
+            label1.Size = new Size(59, 25);
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
@@ -79,12 +84,13 @@
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
             dataGridView1.GridColor = SystemColors.ButtonShadow;
-            dataGridView1.Location = new Point(26, 144);
+            dataGridView1.Location = new Point(22, 120);
+            dataGridView1.Margin = new Padding(2, 2, 2, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 72;
             dataGridView1.RowTemplate.Height = 37;
-            dataGridView1.Size = new Size(1683, 262);
+            dataGridView1.Size = new Size(1402, 218);
             dataGridView1.TabIndex = 7;
             // 
             // Column1
@@ -122,125 +128,148 @@
             groupBox2.Controls.Add(label4);
             groupBox2.Controls.Add(label3);
             groupBox2.Controls.Add(label2);
-            groupBox2.Location = new Point(26, 426);
+            groupBox2.Location = new Point(22, 355);
+            groupBox2.Margin = new Padding(2, 2, 2, 2);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(831, 312);
+            groupBox2.Padding = new Padding(2, 2, 2, 2);
+            groupBox2.Size = new Size(692, 260);
             groupBox2.TabIndex = 8;
             groupBox2.TabStop = false;
             groupBox2.Text = "Tóm tắt thanh toán";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(37, 162);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(182, 25);
+            label5.TabIndex = 3;
+            label5.Text = "Số tiền bệnh nhân trả";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(35, 124);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(182, 25);
+            label4.TabIndex = 2;
+            label4.Text = "Tiền BHYT thanh toán";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(35, 81);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 25);
+            label3.TabIndex = 1;
+            label3.Text = "Mức hưởng BHYT";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(35, 41);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Tổng tiền thuốc";
+            // 
             // groupBox3
             // 
             groupBox3.BackColor = SystemColors.ControlLight;
+            groupBox3.Controls.Add(pictureBox1);
             groupBox3.Controls.Add(label8);
             groupBox3.Controls.Add(label7);
             groupBox3.Controls.Add(label6);
-            groupBox3.Location = new Point(882, 426);
+            groupBox3.Location = new Point(735, 355);
+            groupBox3.Margin = new Padding(2, 2, 2, 2);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(827, 312);
+            groupBox3.Padding = new Padding(2, 2, 2, 2);
+            groupBox3.Size = new Size(689, 260);
             groupBox3.TabIndex = 9;
             groupBox3.TabStop = false;
             groupBox3.Text = "QR thanh toán ";
             // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(350, 145);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 25);
+            label8.TabIndex = 2;
+            label8.Text = "Trạng thái";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(346, 91);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(67, 25);
+            label7.TabIndex = 1;
+            label7.Text = "Số tiền";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(346, 42);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 25);
+            label6.TabIndex = 0;
+            label6.Text = "Nội dung";
+            // 
             // button1
             // 
-            button1.Location = new Point(1117, 761);
+            button1.Location = new Point(882, 634);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(131, 57);
+            button1.Size = new Size(109, 48);
             button1.TabIndex = 10;
             button1.Text = "Hủy";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(1271, 761);
+            button2.Location = new Point(1016, 634);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(131, 57);
+            button2.Size = new Size(158, 48);
             button2.TabIndex = 11;
-            button2.Text = "Tạo";
+            button2.Text = "Tạo QRCode";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(1431, 761);
+            button3.Location = new Point(1192, 634);
+            button3.Margin = new Padding(2, 2, 2, 2);
             button3.Name = "button3";
-            button3.Size = new Size(278, 57);
+            button3.Size = new Size(232, 48);
             button3.TabIndex = 12;
             button3.Text = "Xác nhận đã thanh toán ";
             button3.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // pictureBox1
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(42, 49);
-            label2.Name = "label2";
-            label2.Size = new Size(160, 30);
-            label2.TabIndex = 0;
-            label2.Text = "Tổng tiền thuốc";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(42, 97);
-            label3.Name = "label3";
-            label3.Size = new Size(177, 30);
-            label3.TabIndex = 1;
-            label3.Text = "Mức hưởng BHYT";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(42, 149);
-            label4.Name = "label4";
-            label4.Size = new Size(216, 30);
-            label4.TabIndex = 2;
-            label4.Text = "Tiền BHYT thanh toán";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(44, 194);
-            label5.Name = "label5";
-            label5.Size = new Size(214, 30);
-            label5.TabIndex = 3;
-            label5.Text = "Số tiền bệnh nhân trả";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(483, 51);
-            label6.Name = "label6";
-            label6.Size = new Size(100, 30);
-            label6.TabIndex = 0;
-            label6.Text = "Nội dung";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(483, 109);
-            label7.Name = "label7";
-            label7.Size = new Size(77, 30);
-            label7.TabIndex = 1;
-            label7.Text = "Số tiền";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(487, 174);
-            label8.Name = "label8";
-            label8.Size = new Size(105, 30);
-            label8.TabIndex = 2;
-            label8.Text = "Trạng thái";
+            pictureBox1.Location = new Point(39, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(199, 186);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
             // 
             // ThanhToan
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1746, 845);
+            ClientSize = new Size(1455, 704);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -248,6 +277,7 @@
             Controls.Add(groupBox2);
             Controls.Add(dataGridView1);
             Controls.Add(groupBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "ThanhToan";
             Text = "Thanh toán và in hóa đơn ";
             groupBox1.ResumeLayout(false);
@@ -257,6 +287,7 @@
             groupBox2.PerformLayout();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -281,5 +312,6 @@
         private Label label8;
         private Label label7;
         private Label label6;
+        private PictureBox pictureBox1;
     }
 }
