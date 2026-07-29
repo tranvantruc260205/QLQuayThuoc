eb6e84eeaaafcf0703014325a58e6f341380a3c8
tranvantruc260205 <tranvantruc260205@gmail.com>
2026-07-29 21:15:15 +0700

    Update connection String

--- QLQuayThuoc/Data/AppDbContext.cs
+++ QLQuayThuoc/Data/AppDbContext.cs
@@ -5,8 +5,7 @@ namespace QLQuayThuoc.Data
 {
     public class AppDbContext : DbContext
     {
-        private const string ConnectionString =
-            "server=localhost;port=3307;database=QLQuayThuoc;user=root;password=240405;";     //Tự thay mật khẩu mysql server vào
+        private const string ConnectionString = "";     //Tự thay mật khẩu mysql server vào
 
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<BenhNhan> BenhNhans { get; set; } = null!;
